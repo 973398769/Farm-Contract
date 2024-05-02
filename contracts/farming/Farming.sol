0006f46dd99242ef4da1c86c0eaf787ec5b3e329
小小星球杯 <nini_good@outlook.com>
2024-05-02 20:02:32 +0800

    Add function
    Add function

--- contracts/farming/Farming.sol
+++ contracts/farming/Farming.sol
@@ -21,4 +21,60 @@ contract Farming is Ownable {
         uint256 accERC20PerShare; // Accumulated ERC20s per share, times 1e36.
         uint256 totalDeposit; // Total amount of tokens deposited at the moment (staked)
     }
+    // Address of the ERC20 Token contract.
+    IERC20 public erc20;
+    // The total amount of ERC20 that's paid out as reward.
+    uint256 public paidOut;
+    // ERC20 tokens rewarded per second.
+    uint256 public rewardPerSecond;
+    // Total rewards added to farm
+    uint256 public totalRewards;
+    // Info of each pool.
+    PoolInfo[] public poolInfo;
+    // Info of each user that stakes LP tokens.
+    mapping(uint256 => mapping(address => UserInfo)) public userInfo;
+    // Total allocation points. Must be the sum of all allocation points in all pools.
+    uint256 public totalAllocPoint;
+
+    // The timestamp when farming starts.
+    uint256 public startTimestamp;
+    // The timestamp when farming ends.
+    uint256 public endTimestamp;
+
+    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
+    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
+    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
+
+    constructor(IERC20 _erc20, uint256 _rewardPerSecond, uint256 _startTimestamp) public {
+        erc20 = _erc20;
+        rewardPerSecond = _rewardPerSecond;
+        startTimestamp = _startTimestamp;
+        endTimestamp = _startTimestamp;
+    }
+    // Number of LP pools
+    function poolLength() external view returns (uint256) {
+        return poolInfo.length;
+    }
+    // Fund the farm, increase the end block
+    function fund(uint256 _amount) public {
+        require(block.timestamp < endTimestamp, "fund: too late, the farm is closed");
+        erc20.safeTransferFrom(address(msg.sender), address(this), _amount);
+        endTimestamp += _amount.div(rewardPerSecond);
+        totalRewards = totalRewards.add(_amount);
+    }
+    function add(uint256 _allocPoint, IERC20 _lpToken, bool _withUpdate) public onlyOwner {
+        if (_withUpdate) {
+            massUpdatePools();
+        }
+        uint256 lastRewardTimestamp = block.timestamp > startTimestamp ? block.timestamp : startTimestamp;
+        totalAllocPoint = totalAllocPoint.add(_allocPoint);
+        poolInfo.push(PoolInfo({
+            lpToken : _lpToken,
+            allocPoint : _allocPoint,
+            lastRewardTimestamp : lastRewardTimestamp,
+            accERC20PerShare : 0,
+            totalDeposits : 0
+        }));
+    }
+
 }
