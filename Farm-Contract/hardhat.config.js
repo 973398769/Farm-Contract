3c20be9e6a8a3731f28ea989b0aef1b02284b3c2
小小星球杯 <nini_good@outlook.com>
2024-05-26 22:32:32 +0800

    Add upgrade

--- Farm-Contract/hardhat.config.js
+++ Farm-Contract/hardhat.config.js
@@ -1,7 +1,22 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require('@nomiclabs/hardhat-waffle')
+require('@nomiclabs/hardhat-ethers')
+require("@nomiclabs/hardhat-web3")
+require('@openzeppelin/hardhat-upgrades')
+
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    local: {
+      url: 'http://127.0.0.1:8545',
+      accounts: 'remote'
+    },
+    sepolia: {
+      url: 'https://sepolia.drpc.org',
+      accounts: 'remote'
+    },
+  },
   solidity: {
     version: "0.6.12",
     settings: {
@@ -12,3 +27,4 @@ module.exports = {
     },
   },
 };
+
